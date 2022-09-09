4c1e572686a0336741b5c5061982b2ac0054c1a4
aliakseisatsura <asatsura@pfdrive.com>
2022-09-10 01:11:47 +0400

    add basic data models

---
+++ PasswordApp/DAL/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public  class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Statuses Status { get; set; }
+        public string Password { get; set; }
+        public User User { get; set; }
+    }
+}
