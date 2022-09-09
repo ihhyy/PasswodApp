883bbc6aee90f208610b7f399ad7f42f8172bb72
aliakseisatsura <asatsura@pfdrive.com>
2022-09-10 01:32:55 +0400

    add interface

---
+++ PasswordApp/DAL/Interfaces/IDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IDataRepository
+    {
+        Task<Data> GetAllDataByUserIdAsync(int id);
+        Task<Data> GetDataNameAsync(int id, string name);
+        Task<Data> DeleteDataNameAsync(int id, string name);
+        Task<Data> UpdateDataNameAsync(int id, Data data);
+    }
+}
