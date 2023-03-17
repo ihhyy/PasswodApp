22b92cd005a3d65f76e26a9d64f8282d5c1f72d9
Satsura Aliaksei <Aliaksei.Satsura@wolterskluwer.com>
2023-03-17 19:06:11 +0400

    Add migrations

--- PasswordApp/DAL/Models/Data.cs
+++ PasswordApp/DAL/Models/Data.cs
@@ -14,5 +14,6 @@ namespace DAL.Models
         public string DataValue { get; set; }
         public int UserId { get; set; }
         public int? SecretLength { get; set; }
+        public int? PasswordLifeTime { get; set; }
     }
 }
